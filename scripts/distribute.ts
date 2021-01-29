20bda6fbc835d2884edc08c864f6d8235aba8d71
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-29 10:28:42 +0800

    refactor: workflows

---
+++ scripts/distribute.ts
@@ -0,0 +1,29 @@
+import { Octokit } from '@octokit/rest'
+import { fetchTasks } from './fetch'
+
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+export const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+async function distribute() {
+  const tasks = await fetchTasks()
+
+  for (const task of tasks) {
+    if (!task.enabled)
+      continue
+
+    await octokit.actions.createWorkflowDispatch({
+      owner: 'knightlyjs',
+      repo: 'tasks',
+      // Build, check out https://api.github.com/repos/knightlyjs/tasks/actions/workflows
+      workflow_id: 3030813,
+      ref: 'main',
+      inputs: {
+        tasks: JSON.stringify(task),
+      },
+    })
+
+    console.log(`Scheduled task ${task.owner}/${task.repo}`)
+  }
+}
+
+distribute()
