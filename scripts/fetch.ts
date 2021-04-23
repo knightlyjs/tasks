ffc99db183a4ae6571f9a4e46a79149d83536949
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-23 11:50:37 +0800

    chore: fix

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -4,7 +4,7 @@ import type { KnightlyTask } from 'knightly'
 const GIST = 'https://github.com/knightlyjs/tasks/blob/main/tasks.json?raw=true'
 
 export async function fetchTasks() {
-  const { data } = await axios.get<KnightlyTask[]>(`${GIST}?t=${+new Date()}`)
+  const { data } = await axios.get<KnightlyTask[]>(`${GIST}`)
 
   if (typeof data === 'string')
     throw new Error('Unexpected JSON format')
