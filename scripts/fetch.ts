51ade22e6a40ad596c1f8c72d58fb45bcf2bc81d
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-23 10:30:21 +0800

    chore: update tasks.json

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { KnightlyTask } from 'knightly'
 
-const GIST = 'https://gist.githubusercontent.com/knightly-bot/eaad903eb6a18b78ec5060749ce28683/raw/tasks.json'
+const GIST = 'https://github.com/knightlyjs/tasks/blob/main/tasks.json?raw=true'
 
 export async function fetchTasks() {
   const { data } = await axios.get<KnightlyTask[]>(`${GIST}?t=${+new Date()}`)
