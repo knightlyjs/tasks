4118755a37d932c9ed6d659c539c9e6c77c07226
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-08 16:09:04 +0800

    chore: fix update tasks

--- scripts/update-tasks.ts
+++ scripts/update-tasks.ts
@@ -18,7 +18,7 @@ async function run() {
       `[${task.owner}/${task.repo}](${GITHUB}/${task.owner}/${task.repo})`,
       (task.packagesNameMap ? Object.values(task.packagesNameMap) : [task.publishName])
         .map(name => `[\`${name}\`](${NPM}/package/${name})`).join('<br>'),
-      (task.branches || []).map(b => `[\`${b}\`](${NPM}/package/${task.publishName}/v/${b})`).join(' '),
+      (task.branches || []).map(b => `[\`${b}\`](${NPM}/package/${task.publishName}/v/${paramCase(b)})`).join(' '),
       (task.pulls || []).map(p => `[#${p}](${NPM}/package/${task.publishName}/v/pr${p})`).join(' '),
     ])
   }
@@ -34,6 +34,10 @@ ${table.map(i => `| ${i.join(' | ')} |`).join('\n')}
   readme = readme.replace(REGEX, `<!--TASKS-START-->\n${tableMD}\n<!--TASKS-END-->`)
 
   await fs.writeFile('README.md', readme, 'utf-8')
+}
+
+function paramCase(str) {
+  return str.replace(/[A-Z]/g, s => `-${s.toLowerCase()}`)
 }
 
 run()
